b1072e663c18646af8baa8e30333b40817cfdf21
cameron fuller <cameronmfuller@yahoo.com>
2018-01-07 23:07:50 -0500

    twitter function

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.WXx0UJdOOe9ICUiwpxnRWMxIH,
-  consumer_secret: process.env.p4euxCulWEIMtdWWRn3BUvgty9ckPFPLRTuPgYX3TR8bawbqdX,
-  access_token_key: process.env.950156176909590528-5D9CO4ztfk46s87BMreX4mJaZAMZLV2,
-  access_token_secret: process.env.TA1ZZFOfAQBDWtAlRpO9DLaiJWK9hx13xY6jniI27HU0R
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.44a477d02589470c8f5a2dabb2e735cf,
-  secret: process.env.7c8d4557151c40509a622270d14e4284
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var fs = require("fs");
 
 // command
@@ -20,4 +21,30 @@ switch (input) {
     case "do-what-it-says":
         doWhat();
         break;
+}
+
+// twitter function
+
+function getTwitter() {
+    var Twitter = require("twitter");
+    var keys = require("./keys.js");
+    var client = new Twitter(keys.twitter);
+
+    var params = {
+        screen_name: "_lux__",
+        count: 20
+    };
+
+    client.get('statuses/user_timeline', params, function (error, data) {
+        if (error);
+        console.log(error);
+
+        for (i = 0; i < data.length; i++) {
+            console.log(data[i].text)
+            fs.appendFile("log.txt", data[i].text + "\n", function (errorTwo) {
+                if (errorTwo)
+                    console.log(errorTwo);
+            });
+        }
+    });
 }
