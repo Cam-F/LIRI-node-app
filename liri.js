7f490fca7dc4f2dfb45124cc1833c8205a392583
cameron fuller <cameronmfuller@yahoo.com>
2018-01-08 00:25:46 -0500

    spotify

--- liri.js
+++ liri.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 var fs = require("fs");
+var request = require("request");
 
 // command
 var input = process.argv[2];
+var search = "";
+//loop to combine the multi word searches
+for (i = 3; i < process.argv.length; i++) {
+    search += (process.argv[i] + "+");
+}
 
 // switch
 switch (input) {
@@ -39,6 +45,8 @@ function getTwitter() {
         if (error);
         console.log(error);
 
+        fs.appendFile("log.txt", "----- My Tweets -----\n");
+
         for (i = 0; i < data.length; i++) {
             console.log(data[i].text)
             fs.appendFile("log.txt", data[i].text + "\n", function (errorTwo) {
@@ -46,5 +54,55 @@ function getTwitter() {
                     console.log(errorTwo);
             });
         }
+    });
+}
+
+// spotify function
+
+function getSpotify() {
+
+    // search convert
+    if (search == "") {
+        var songSearch = "The Sign artist:Ace of Base";
+    } else {
+        var songSearch = '"' + search + '"';
+    }
+
+    var Spotify = require("node-spotify-api");
+    var keys = require("./keys.js");
+    var spotify = new Spotify({
+        id: '44a477d02589470c8f5a2dabb2e735cf',
+        secret: '7c8d4557151c40509a622270d14e4284'
+    });
+
+    // searching the spotify api
+    spotify.search({ type: 'track', query: songSearch, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        // console logs song info
+        var songInfo = data.tracks.items[0];
+        console.log("Artist: ", songInfo.artists[0].name);
+        console.log("Song: ", songInfo.name);
+        console.log("Preview Link: ", songInfo.preview_url);
+        console.log("Album: ", songInfo.album.name);
+
+        // appending to log.txt
+        fs.appendFile("log.txt", "----- Spotify -----\n", function (error) {
+            if (error) throw error;
+        });
+        fs.appendFile("log.txt", "Artist: " + songInfo.artists[0].name + "\n", function (error) {
+            if (error) throw error;
+        });
+        fs.appendFile("log.txt", "Song: " + songInfo.name + "\n", function (error) {
+            if (error) throw error;
+        });
+        fs.appendFile("log.txt", "Preview Link: " + songInfo.preview_url + "\n", function (error) {
+            if (error) throw error;
+        });
+        fs.appendFile("log.txt", "Album: " + songInfo.album.name + "\n", function (error) {
+            if (error) throw error;
+        });
     });
 }
