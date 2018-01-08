bfe94427c340e04e7a3f6366ad43786bd8f57db4
cameron fuller <cameronmfuller@yahoo.com>
2018-01-07 21:16:30 -0500

    switch

--- liri.js
+++ liri.js
@@ -1,1 +1,23 @@
 var fs = require("fs");
+
+// command
+var input = process.argv[2];
+
+// switch
+switch (input) {
+    case "my-tweets":
+        getTwitter();
+        break;
+
+    case "spotify-this-song":
+        getSpotify();
+        break;
+
+    case "movie-this":
+        getMovie();
+        break;
+
+    case "do-what-it-says":
+        doWhat();
+        break;
+}
