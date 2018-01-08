2374fd89f9bf76e8a20a0c00709ebbc692b7e6c2
cameron fuller <cameronmfuller@yahoo.com>
2018-01-07 21:01:58 -0500

    init files

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.WXx0UJdOOe9ICUiwpxnRWMxIH,
+  consumer_secret: process.env.p4euxCulWEIMtdWWRn3BUvgty9ckPFPLRTuPgYX3TR8bawbqdX,
+  access_token_key: process.env.950156176909590528-5D9CO4ztfk46s87BMreX4mJaZAMZLV2,
+  access_token_secret: process.env.TA1ZZFOfAQBDWtAlRpO9DLaiJWK9hx13xY6jniI27HU0R
+};
+
+exports.spotify = {
+  id: process.env.44a477d02589470c8f5a2dabb2e735cf,
+  secret: process.env.7c8d4557151c40509a622270d14e4284
+};
